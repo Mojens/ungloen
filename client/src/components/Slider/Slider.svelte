7efc66763b5e7f724b4805f158bb7b643e1a9fb4
mojens <moha135h@stud.kea.dk>
2023-05-03 20:35:36 +0200

    Slider component

---
+++ client/src/components/Slider/Slider.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { fade } from "svelte/transition";
+  
+    export let carouselPhotos = [];
+  
+    let index = 0;
+  
+    const next = () => {
+      index = (index + 1) % carouselPhotos.length;
+    };
+  
+    setInterval(next, 5000);
+  </script>
+  
+  {#each carouselPhotos as { src, alt }, i}
+    {#if i === index}
+      <img transition:fade={{ delay: 0, duration: 0 }} src={src} alt={alt} />
+    {/if}
+  {/each}
+
+  
--- client/src/pages/Home/Home.svelte
+++ client/src/pages/Home/Home.svelte
@@ -1,120 +1,27 @@
 <script>
-
+  import Slider from "../../components/Slider/Slider.svelte";
+  const carouselPhotos = [
+      {
+        src:
+          "https://images.pexels.com/photos/1602726/pexels-photo-1602726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        alt: "Blå penge gris",
+      },
+      {
+        src:
+          "https://images.pexels.com/photos/3769747/pexels-photo-3769747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        alt: "Pige med shopping poser, og shopper online",
+      },
+      {
+        src:
+          "https://images.pexels.com/photos/853151/pexels-photo-853151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        alt: "Kæreste par der shopper online",
+      },
+    ];
 </script>
-<main class="container">
-    <h1>Hello</h1>
-    <div class="grid">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-      </div>
-      <button>Button</button>
-<input type="submit">
-<form>
 
-    <!-- Grid -->
-    <div class="grid">
-  
-      <!-- Markup example 1: input is inside label -->
-      <label for="firstname">
-        First name
-        <input type="text" id="firstname" name="firstname" placeholder="First name" required>
-      </label>
-  
-      <label for="lastname">
-        Last name
-        <input type="text" id="lastname" name="lastname" placeholder="Last name" required>
-      </label>
-  
-    </div>
-  
-    <!-- Markup example 2: input is after label -->
-    <label for="email">Email address</label>
-    <input type="email" id="email" name="email" placeholder="Email address"  required>
-    <small>We'll never share your email with anyone else.</small>
-  
-    <!-- Button -->
-    <button type="submit">Submit</button>
-  
-  </form>
-
-  <table>
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-      </tr>
-      <tr>
-        <th scope="row">1</th>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-      </tr>
-      <tr>
-        <th scope="row">1</th>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-        <td>Cell</td>
-      </tr>
-    </tbody>
-    <tfoot>
-      <tr>
-        <th scope="col">#</th>
-        <td scope="col">Total</td>
-        <td scope="col">Total</td>
-        <td scope="col">Total</td>
-        <td scope="col">Total</td>
-        <td scope="col">Total</td>
-      </tr>
-    </tfoot>
-  </table>
-
-  <div class="grid">
-    <div>
-        <article>
-            <header>Header</header>
-            Body
-            <footer>Footer</footer>
-          </article>
-    </div>
-    <div>
-        <article>
-            <header>Header</header>
-            Body
-            <footer>Footer</footer>
-          </article>
-    </div>
-    <div>
-        <article>
-            <header>Header</header>
-            Body
-            <footer>Footer</footer>
-          </article>
-    </div>
+<main>
+  <Slider carouselPhotos={carouselPhotos} />
+  <div class="container">
+    <h1>Home</h1>
   </div>
-  <progress></progress>
-  <button aria-busy="true">Please wait…</button>
-<button aria-busy="true" class="secondary"></button>
-<p>Tooltip on a <a href="#" data-tooltip="Tooltip">link</a></p>
-<p>Tooltip on <em data-tooltip="Tooltip">inline element</em></p>
-<p><button data-tooltip="Tooltip">Tooltip on a button</button></p>
 </main>
