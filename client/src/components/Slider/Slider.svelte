cd61cad4b295155028cfbba816991aa61a0c6e49
mojens <moha135h@stud.kea.dk>
2023-05-13 02:24:03 +0200

    bit of styling for slider

--- client/src/components/Slider/Slider.svelte
+++ client/src/components/Slider/Slider.svelte
@@ -3,12 +3,30 @@
     export let carouselPhotos = [];
 </script>
 
-<div class="slider-container center" style="max-width: 1000px; margin:0 auto;">
-    <Carousel autoplay autoplayDuration={3500} arrows={false} dots={false} infinite={true}>
+<div class="slider-container center">
+    <Carousel
+        autoplay
+        autoplayDuration={3500}
+        arrows={false}
+        dots={false}
+        infinite={true}
+    >
         {#each carouselPhotos as { src, alt }, i}
-            <img class="slider-img" {src} {alt} style="max-width: 1000px; height: 550px;"/>
+            <img class="slider-img" {src} {alt} />
         {/each}
     </Carousel>
 </div>
 
-
+<style>
+    .slider-container {
+        margin: 0 auto;
+        width: 100%;
+        height: 550px;
+        overflow: hidden;
+    }
+    .slider-img {
+        width: 100%;
+        height: 550px;
+        object-fit: cover;
+    }
+</style>
